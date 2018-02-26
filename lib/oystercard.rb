95e24ad5d07eeca796f5b86a518adbc87cdb3bae
Charlene Bastians <charlenebastians@hotmail.com>
2018-02-26 07:44:30 -0800

    maximum balance

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,7 +1,14 @@
 class Oystercard
-  attr_reader :balance
+  attr_reader :balance, :maximum_balance
+  MAXIMUM_BALANCE = 90
 
   def initialize
     @balance = 0
+    @maximum_balance =  MAXIMUM_BALANCE
+  end
+
+  def top_up(amount)
+    raise "Maximum balance of #{maximum_balance} exceeded" if amount + balance > MAXIMUM_BALANCE
+    @balance += amount
   end
 end
