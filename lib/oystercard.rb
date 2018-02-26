aa36f6b6fb1089dcfeaca9e9cd5299fb23b39efe
Charlene Bastians <charlenebastians@hotmail.com>
2018-02-26 08:49:11 -0800

    touch in and out mehtods

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,10 +1,11 @@
 class Oystercard
-  attr_reader :balance, :maximum_balance
+  attr_reader :balance, :maximum_balance, :state_of_journey
   MAXIMUM_BALANCE = 90
 
   def initialize
     @balance = 0
     @maximum_balance =  MAXIMUM_BALANCE
+    @state_of_journey = :out
   end
 
   def top_up(amount)
@@ -14,5 +15,17 @@ class Oystercard
 
   def deduct(amount)
     @balance -= amount
+  end
+
+  def touch_in
+    @state_of_journey = :in
+  end
+
+  def touch_out
+    @state_of_journey = :out
+  end
+
+  def in_journey?
+    state_of_journey == :in
   end
 end
